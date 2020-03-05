ef412faa96bab36dd808073a6f058193ba222bab
yanismoula <moulayanis@gmail.com>
2020-03-05 11:27:26 +0100

    test

---
+++ acceuil.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Medi-Sys</title>
+    <link rel="stylesheet" type="text/css" href="styleAcceuil.css">
+
+</head>
+<body>
+<header id="pdg">
+    <h1>
+        Avant le décollage
+    </h1>
+    <p>
+        Lorem500
+    </p>
+    <a href="#" class="bouton">Se</a>
+
+</header>
+
+
+
+</body>
+</html>
