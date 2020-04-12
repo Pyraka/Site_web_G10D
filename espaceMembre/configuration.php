8382f0e4608b1cbd5e7736e77892091566a10893
keyservlad <58270823+keyservlad@users.noreply.github.com>
2020-04-12 16:04:42 +0200

    version à jour (mise en commun arnaud et jerem)

---
+++ espaceMembre/configuration.php
@@ -0,0 +1,10 @@
+<?php
+
+session_start();
+ 
+$bdd = new PDO('mysql:host=localhost;dbname=infinite_;charset=utf8', 'root', '');
+ 
+include('cookieConnect.php');
+
+
+?>
---
+++ espaceMembre/index.php
@@ -0,0 +1,19 @@
+<?php include "configuration.php";
+include "templates/header.php"; ?>
+
+
+<ul>
+  <li>
+    <a href="create.php"><strong>S'inscrire</strong></a> - inscription de l'utilisateur
+  </li>
+  <li>
+    <a href="connect.php"><strong>Se connecter</strong></a> - connexion de l'utilisateur
+  </li>
+  <li>
+    <a href="read.php"><strong>Chercher</strong></a> - chercher un utilisateur
+  </li>
+</ul>
+
+<?php include "templates/footer.php"; ?>
+  </body>
+</html>
