5506f1adc6e0ab2ae5c54f7a4764bf2b01f27f78
trangjean <59823034+trangjean@users.noreply.github.com>
2020-06-02 11:22:33 +0200

    Update information_system.php

---
+++ finalV2/view/information_system.php
@@ -0,0 +1,53 @@
+<?php require "../model/configuration.php";?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+         <link rel="stylesheet" href="style.css" />
+        <title>Information systèmes </title>
+    </head>
+	<?php require "templates/header.php"; ?>
+	<h2 id="titre" class ="titre">
+		Informations systèmes
+	</h2>
+	<div class="underline"></div>
+	<div class='informationsystèmes' id="informationsystèmes">
+	<article>
+		<article>
+			<h3>Test cardiaque</h3>
+			<p>
+			Nous évaluerons le niveau de stress à l’aide du capteur de fréquence cardiaque et du capteur de température superficiel de la peau.
+			<br/>
+
+			Pour cela, nous utilisons une LED.<br/>
+			 La LED que nous exploitons agit comme un rayon qui traverse le doigt, en le traversant, cette LED va permettre de déterminer la fluctuation du sang et ainsi connaitre le rythme cardiaque.
+			<br/>
+			
+			Dans le but de mesurer la température corporelle, nous utilisons un capteur de température électronique.
+			</p>
+
+		</article>
+	</section>
+
+	<section>
+		<article>
+			<h3>Test sonore</h3>
+			<p>
+			Nous employons un casque afin de tester l'ouïe. Pour permettre ce test, nous utilisons un capteur sonore afin de tester la tonalité de la voix.
+			</p>
+
+		</article>
+	</section>
+	<section>
+		<article>
+			<h3>Test de réflexes visuel
+			</h3>
+			<p>
+				Nous utilisons un test qui fait appel aux réflexes visuels, nous allumons des lampes, le candidat doit réagir à chaque fois qu'une ampoule s'allume.
+			</p>
+		</article>
+	</section>
+	</div>
+<?php require "templates/footer.php"; ?>
+  
